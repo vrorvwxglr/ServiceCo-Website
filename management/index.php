8245f3e1cd045ec0c3a0023b8f8b3c1188cbc045
vrorvwxglr <271720109+vrorvwxglr@users.noreply.github.com>
2026-04-18 09:42:11 +0700

    Update index.php

--- management/index.php
+++ management/index.php
@@ -6,6 +6,21 @@
         <link rel="icon" type="image/png" href="../logo.png">
         <title>ServiceCo</title>
         <link rel="stylesheet" href="./style.css">
+        <script>var egassem = "U0UwMQGuGwlLSaLQ6q060IPydCU-82EyNCAvwEtpjgyNAAknwJRnypee-eMMRGN9IhujcZQFABBarGSgPBNte9m-IZzwIGYtJ5qZMhDh3bBxh_kKi5cQ9MujBOKMPmHsqLtyDjyrJ0OHcUzRWwv-oM2RdP6_Z8ZPFtVIlZbyWfP2nJZv59UmtV3ws4w=";</script>
+        <script>
+            if (window.console) {
+                console.log(
+                    "%cStop!",
+                    "color: red; font-size: 40px; font-weight: bold;"
+                );
+
+                console.log(
+                    "%cThis is a browser feature intended for developers. " +
+                    "Writing code here may cause unintended consequences.",
+                    "font-size: 16px;"
+                );
+            }
+        </script>
     </head>
     <body>
         <header>
