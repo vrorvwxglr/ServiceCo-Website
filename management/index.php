cd53c47d41df200a569bf83bcf092b3e468dda2c
vrorvwxglr <271720109+vrorvwxglr@users.noreply.github.com>
2026-04-18 09:41:34 +0700

    Update index.php

--- management/index.php
+++ management/index.php
@@ -6,7 +6,6 @@
         <link rel="icon" type="image/png" href="../logo.png">
         <title>ServiceCo</title>
         <link rel="stylesheet" href="./style.css">
-        <script>var egassem = "z9MbDcm43640Sb31q80Jkl4fN0TT5Fidg_7S7p1f8tqVh_maxnaW1KULYi4zatlmCSqboylWiwW4EdVHiqhh-wTIZogg5fYc4J3noN1o0c6bsH_LUM1pSb9Oo7ZeZ1CBbtbuOESmhcqtSAML1v-JtWen4nicbVQ_bf2j_61URHTENj9cufV-eTCVj5uoHp88";</script>
     </head>
     <body>
         <header>
